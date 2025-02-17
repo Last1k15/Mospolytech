c05a5c171fad2a2285e355fecf4b3eb0b860dd49
Last1k15 <161221631+Last1k15@users.noreply.github.com>
2025-02-17 22:52:09 +0300

    Update mainwindow.cpp

--- Qt/hello_qt_window/mainwindow.cpp
+++ Qt/hello_qt_window/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextCodec>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QTextCodec *codec = QTextCodec::codecForName("CP1251");
+    QTextCodec::setCodecForLocale(codec);
+    setWindowTitle("Привет, Qt!");
+    resize(700, 500);
 }
 
 MainWindow::~MainWindow()
